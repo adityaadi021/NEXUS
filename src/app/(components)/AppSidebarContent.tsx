9d9de467a7d7471cad935487bbc70037b151253b
adityaadi021 <adityabugalia063@gmail.com>
2025-06-17 17:05:49 +0530

    Delete src directory

--- src/app/(components)/AppSidebarContent.tsx
+++
@@ -1,4 +0,0 @@
-
-// This file is no longer needed and its content has been cleared.
-// The navigation has been moved back to the main header.
-// If this file should be deleted from the project, please provide specific instructions to do so.

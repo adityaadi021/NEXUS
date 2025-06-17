72f9ac67f9571c31495c2eec12c1b2b2d5e64511
adityaadi021 <adityabugalia063@gmail.com>
2025-06-17 17:05:15 +0530

    Delete .next directory

--- .next/static/chunks/src_app_admin_dashboard_page_tsx_f2e058b7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_18440c22._.js",
-  "static/chunks/node_modules_6fcb386a._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_tournaments_[id]_page_tsx_5e0b58e5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_beaad824._.js",
-  "static/chunks/src_components_9c60c84e._.js"
-],
-    source: "dynamic"
-});
